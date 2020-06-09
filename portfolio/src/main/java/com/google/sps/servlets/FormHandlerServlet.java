76971db7297fd0915de05bfe2a13cbc235219999
Alvin Hermans <halvin@google.com>
2020-06-09 19:06:01 +0000

    upload url to datastore

---
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -0,0 +1,93 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.blobstore.BlobInfo;
+import com.google.appengine.api.blobstore.BlobInfoFactory;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.ServingUrlOptions;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/* Servlet that processes file URL from Blobstore */
+@WebServlet("/form-handler")
+public class FormHandlerServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // URL of user-uploaded image on blobstore
+    String imageURL = getUploadedFileUrl(request, "image");
+
+    // Upload url to datastore
+    long timeStamp = System.currentTimeMillis();
+
+    Entity imageEntity = new Entity("Image");
+    imageEntity.setProperty("time", timeStamp);
+    imageEntity.setProperty("url", imageURL);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(imageEntity);
+  }
+
+  /* Returns URL that points to user-uploaded file, or null if user didn't upload an image */
+  private String getUploadedFileUrl(HttpServletRequest request, String inputElementName) {
+    BlobstoreService blobstore = BlobstoreServiceFactory.getBlobstoreService();
+    System.out.println("Blobstore made");
+    Map<String, List<BlobKey>> blobs = blobstore.getUploads(request);
+    System.out.println("blobs acquired");
+    List<BlobKey> blobKeys = blobs.get(inputElementName);
+    System.out.println("blobkeys made");
+
+    if (blobKeys == null || blobKeys.isEmpty()) {
+      return null;
+    }
+    
+    // Our form only contains a single file input, so get the first index.
+    BlobKey blobKey = blobKeys.get(0);
+
+    // User submitted form without selecting a file, so we can't get a URL.
+    BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+    if (blobInfo.getSize() == 0) {
+      blobstore.delete(blobKey);
+      return null;
+    }
+
+    // Use ImagesService to get a URL that points to the uploaded file.
+    ImagesService imagesService = ImagesServiceFactory.getImagesService();
+    ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
+
+    try {
+      URL url = new URL(imagesService.getServingUrl(options));
+      return url.getPath();
+    } catch (MalformedURLException e) {
+      return imagesService.getServingUrl(options);
+    }
+  }
+}
